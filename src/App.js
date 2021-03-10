d59b718321f5468328faec3e6727f03f2ce5f9e7
Ashish <kapoleashish.76@gmail.com>
2021-03-10 22:16:17 +0530

    Edit and Delet functions Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import  { React , Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class Appp extends Component{
+constructor(){
+  super();
+  this.state={
+    employeeData : []
+  }
 }
 
-export default App;
+handleSubmit=(e)=>{
+  e.preventDefault();
+  let employeeData = this.state.employeeData;
+  let Title = this.refs.txtTitle.value;
+  let Description = this.refs.txtDescription.value;
+
+  let newEmployee = {
+    "Title" : Title,
+    "Description" : Description
+  }
+
+  employeeData.push(newEmployee);
+  this.setState(
+    {
+      employeeData: employeeData
+    }
+  )
+
+  this.refs.myForm.reset();
+}
+
+// handleEdit=(i)=>{
+
+// }
+
+
+
+// handleDelete=(i)=>{
+//   let employeeData=this.state.employeeData;
+//   employeeData.splice(i,1);
+//   this.setState({
+//     employeeData:employeeData
+//   });
+// }
+
+render(){
+  let employeeData= this.state.employeeData;
+  return(
+    <div>
+    <form ref ="myForm">
+
+<label>Title</label>
+<input type="text" ref="txtTitle" placeholder="Enter Tittle"></input>
+   
+   
+   <label>Description</label>
+   <input type ="text area" ref="txtDescription" placeholder="Enter Description"/>
+   <button onClick={e => this.handleSubmit(e)}>Save</button>
+   
+    </form>
+<table>
+<tr>
+  <th>Title</th>
+  <th>Description</th>
+</tr>
+{
+  employeeData.map((data , i) =>
+  <tr key ={i}>
+    <td>{data.Title}</td>
+    <td>{data.Description}</td>
+     {/* <td>
+
+      <button onClick={this.handleEdit(i)}>Edit</button>
+    
+     </td>
+     <td>
+     
+      <button onClick={this.handleDelete(i)}>Delete</button>
+      
+    </td> */}
+
+  </tr>
+  )
+}
+</table>
+
+</div>
+
+
+     )
+}  
+}
+export default Appp;
